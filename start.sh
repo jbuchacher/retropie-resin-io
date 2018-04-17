a20c7b4dd83caf2e204856a5903c929d77d03e42
Joshua Buchacher <jbuchacher@me.com>
2018-04-16 17:42:53 -0700

    Install desktop

--- start.sh
+++ start.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-# Start resin-wifi-connect
-export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
-sleep 1
-node /wifi/resin-wifi-connect/src/app.js --clear=false
-
-# At this point the WiFi connection has been configured and the device has
-# internet - unless the configured WiFi connection is no longer available.
-
-# run retropie setup
 /RetroPie-Setup/RetroPie-Setup/retropie_setup.sh
