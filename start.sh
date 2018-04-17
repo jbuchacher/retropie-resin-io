ee7220e89d0aa6af48c2903d426f8d4d8731694d
Joshua Buchacher <jbuchacher@me.com>
2018-04-16 21:07:37 -0700

    test

--- start.sh
+++ start.sh
@@ -2,8 +2,6 @@
 
 adduser --disabled-password --gecos "" pi
 
-for GROUP in adm dialout cdrom sudo audio video plugdev games users netdev input spi i2c gpio sudo tty; do adduser pi $GROUP; done
+for GROUP in adm dialout cdrom sudo audio video plugdev games users netdev input spi i2c gpio tty; do adduser pi $GROUP; done
 
-rm /tmp/.X0-lock &>/dev/null || true
-
-su -l pi -c startx
+startx
