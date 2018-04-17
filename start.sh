da2c19504654c11f9162008771bbe65f5e2b955a
Joshua Buchacher <jbuchacher@me.com>
2018-04-16 20:59:30 -0700

    fix?

--- start.sh
+++ start.sh
@@ -4,4 +4,6 @@ adduser --disabled-password --gecos "" pi
 
 for GROUP in adm dialout cdrom sudo audio video plugdev games users netdev input spi i2c gpio sudo tty; do adduser pi $GROUP; done
 
+rm /tmp/.X0-lock &>/dev/null || true
+
 su -l pi -c startx
