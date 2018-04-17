3d11debd0ed72bd8b015e7acdb84032a3a7e6494
Joshua Buchacher <jbuchacher@me.com>
2018-04-16 21:23:55 -0700

    test

--- start.sh
+++ start.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-adduser --disabled-password --gecos "" pi
-
-for GROUP in adm dialout cdrom sudo audio video plugdev games users netdev input spi i2c gpio tty; do adduser pi $GROUP; done
-
 startx
