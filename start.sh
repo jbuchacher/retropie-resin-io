f96c54b33c70698280f89985a331e8e2c9d917b0
Joshua Buchacher <jbuchacher@me.com>
2018-04-16 20:12:39 -0700

    test

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 adduser --disabled-password --gecos "" pi
-usermod -a -G video pi
 
-su -u pi
+for GROUP in adm dialout cdrom sudo audio video plugdev games users netdev input spi i2c gpio; do adduser pi $GROUP; done
+
+su pi
 
 /RetroPie-Setup/RetroPie-Setup/retropie_setup.sh
